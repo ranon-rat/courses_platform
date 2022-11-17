e02306f009136e517b6da3b29820564ca342e9bf
FlamesX-128 <flamesx128@gmail.com>
2022-11-17 15:45:59 -0600

    planning the structure of the frontend.

--- src/router/setup.go
+++ src/router/setup.go
@@ -4,8 +4,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/bruh-boys/courses_platform/src/controllers"
+	"github.com/gomarkdown/markdown"
 )
 
 func SetupRouter() error {
@@ -13,6 +15,17 @@ func SetupRouter() error {
 	http.HandleFunc("/sign-up", controllers.SignUp)
 	http.HandleFunc("/api", controllers.ApiInformation)
 	http.HandleFunc("/new-post", controllers.NewPost)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		file, _ := os.ReadFile("public/index.html")
+		md, _ := os.ReadFile("public/test.md")
+		out := markdown.ToHTML(md, nil, nil)
+
+		stri := strings.Replace(string(file), "!#!", string(out), 1)
+
+		w.Write([]byte(stri))
+	})
+
 	port, exist := os.LookupEnv("PORT")
 	if !exist {
 		log.Println("debuggin mode,working in the port 8080")
