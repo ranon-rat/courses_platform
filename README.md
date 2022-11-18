9f5c316af968a9ab61dc672b15515e25ed800def
FlamesX-128 <flamesx128@gmail.com>
2022-11-18 04:02:46 -0600

    home view added.

--- README.md
+++ README.md
@@ -1,33 +1,14 @@
 # Course platform
 
-Foobar is a Python library for dealing with word pluralization.
+A platform for uploading courses.
+
+## Visualization
+
+
 
 ## Build requirements
 
 - [Go 1.19.x](https://go.dev/dl/)
-
-## Installation
-
-Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
-
-```bash
-pip install foobar
-```
-
-## Usage
-
-```python
-import foobar
-
-# returns 'words'
-foobar.pluralize('word')
-
-# returns 'geese'
-foobar.pluralize('goose')
-
-# returns 'phenomenon'
-foobar.singularize('phenomena')
-```
 
 ## Contributing
 
@@ -35,7 +16,3 @@ Pull requests are welcome. For major changes, please open an issue first
 to discuss what you would like to change.
 
 Please make sure to update tests as appropriate.
-
-## License
-
-[MIT](https://choosealicense.com/licenses/mit/)
--- src/client/scripts/main.ts
+++ src/client/scripts/main.ts
@@ -1,10 +1,18 @@
-"use strict";
 const url = new URL(window.location.href);
 const opt = url.searchParams.get('id');
+
 if (url.pathname === '/' && opt === null) {
     window.location.href = '?id=1';
 }
-async function search(query) {
+
+interface Response {
+    publications: {
+        title: string;
+        id: string;
+    }[];
+}
+
+async function search(query: string): Promise<Response['publications']> {
     const resp = await fetch('/api?page=1', {
         method: 'GET',
         headers: {
@@ -12,44 +20,59 @@ async function search(query) {
             'Accept': 'application/json',
         }
     });
-    const data = await resp.json();
+
+    const data: Response = await resp.json();
     const keys = query.split(' ');
-    return (data.publications || []).filter((item) => keys.every((key) => item.title.toLowerCase().includes(key)));
+
+    return (data.publications || []).filter((item) => 
+        keys.every((key) => item.title.toLowerCase().includes(key))
+    )
 }
-const input = document.getElementById('search');
+
+const input = document.getElementById('search')!
+
 input.addEventListener('input', function () {
-    const group = document.getElementById('search-group');
+    const group = document.getElementById('search-group')!;
     group.innerHTML = '';
+    
     // @ts-ignore
     const query = this.value.toLowerCase();
-    if (query.length < 2)
-        return group.innerHTML = '';
+
+    if (query.length < 2) return group.innerHTML = '';
+
     search(query).then((data) => {
         for (let i = 0; i < data.length && i < 5; i++) {
             const item = data[i];
+
             group.innerHTML += `
                 <li class="form-control p-0 m-1">
                     <a class="nav-link active" aria-current="page" href="/?id=${item.id}">${item.title}</a>
                 </li>
             `;
         }
-    });
-});
+    })
+})
+
 input.addEventListener('blur', function () {
-    const group = document.getElementById('search-group');
+    const group = document.getElementById('search-group')!;
     setTimeout(() => group.innerHTML = '', 200);
-});
+})
+
 // tag, title
-let titles = [];
+let titles: [string, string][] = [];
+
 for (const tag of ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']) {
-    const elements = document.getElementsByTagName(tag);
+    const elements = document.getElementsByTagName(tag) as HTMLCollectionOf<HTMLElement>;
+
     for (let i = 0; i < elements.length; i++) {
         const element = elements[i]; // @ts-ignore
         element.setAttribute('id', `${element.innerHTML}`);
         titles.push([element.id, element.innerText]);
     }
 }
-const sidebar = document.getElementById('sidebar');
+
+const sidebar = document.getElementById('sidebar')!;
+
 for (const [id, title] of titles) {
     sidebar.innerHTML += `
         <li class="list-group-item">
