c2df5dd39b3b29e505a8228212ee1df3ebeb6e64
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2022-11-23 15:19:56 -0600

    bruh

--- src/router/setup.go
+++ src/router/setup.go
@@ -3,7 +3,6 @@ package router
 import (
 	"log"
 	"net/http"
-	"os"
 	"path"
 
 	"github.com/bruh-boys/courses_platform/src/controllers"
@@ -14,23 +13,48 @@ var TemplateDirectory = path.Join("src", "templates")
 
 func SetupRouter(dirBase string, port string) (err error) {
 	router := mux.NewRouter().StrictSlash(true)
-	controllers.Setup()
-	router.HandleFunc("/sign-in", controllers.SignIn)
-	router.HandleFunc("/sign-up", controllers.SignUp)
-	router.HandleFunc("/api", controllers.ApiInformation)
-	router.HandleFunc("/new-post", controllers.NewPost)
+	path := path.Join(dirBase, TemplateDirectory)
 
+	if controllers.Templates, err = controllers.InitializeTemplates(path, controllers.TemplateFuncs); err != nil {
+
+		return
+	}
+
+	// Static files.
 	router.HandleFunc(`/public/{file:[\w\W\/]+}`, func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, r.URL.Path[1:])
 	})
+
+	// Initialize API routes.
+	// router.HandleFunc("/api/auth/sign", controllers.Post) // DELETE - Sign out.
+	// router.HandleFunc("/api/auth/sign", controllers.Post) // POST - Sign up.
+	// router.HandleFunc("/api/auth/sign", controllers.Post) // PUT - Sign in.
+
+	// router.HandleFunc("/api/post/{id:[0-9]+}", controllers.Post) // DELETE - Delete post.
+	// router.HandleFunc("/api/post/{id:[0-9]+}", controllers.Post) // GET - Get post.
+
+	// router.HandleFunc("/api/posts", controllers.Post) // GET - Get posts.
+
+	// router.HandleFunc("/api/post", controllers.Post) // POST - Create post.
+
+	// Initialize routes.
+	// router.HandleFunc("/auth/signin", controllers.SignIn)
+	// router.HandleFunc("/auth/signup", controllers.SignUp)
+
+	// router.HandleFunc("/post/{id:[0-9]+}", controllers.Post)
+	// router.HandleFunc("/", controllers.RenderHome)
+
+	router.HandleFunc("/sign-in", controllers.SignIn)
+	router.HandleFunc("/sign-up", controllers.SignUp)
+	router.HandleFunc("/api", controllers.ApiInformation)
+
+	router.HandleFunc("/new-post", controllers.NewPost)
+
 	router.HandleFunc("/post", controllers.GetPost)
 	router.HandleFunc("/", controllers.RenderHome)
 
-	port, exist := os.LookupEnv("PORT")
-	if !exist {
-		log.Println("debuggin mode,working in the port 8080")
-		port = "8080"
-	}
+	// Start server.
+	log.Println("Starting server on port " + port + "...")
 
-	return http.ListenAndServe(":"+port, router)
+	return http.ListenAndServe((":" + port), router)
 }
